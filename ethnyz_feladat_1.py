b78b8b7655e438576c839ad0b3662c3e8fd0139a
ETHNYZ <a@a.com>
2024-02-08 15:00:06 +0100

    haliho

---
+++ ethnyz_feladat_1.py
@@ -0,0 +1,8 @@
+# Legkisebb szam meghatrozasa
+
+# Számok bekérése
+szam1 = input("Kérem az első számot: ")
+szam2 = input("Kérem a masodik számot: ")
+szam3 = input("Kérem a harmadik számot: ")
+
+print("A legkisebb szam:" + min(szam1, szam2, szam3))
