fb77ae4d72fbe806847bd4b223391076bf6f8c93
etotist <4@4.com>
2024-02-08 15:01:14 +0100

    :o

---
+++ etotist_felcsi_1.py
@@ -0,0 +1,10 @@
+# Írj egy Python programot, amely bekér három számot a felhasználótól
+# és kiírja a képernyőre a legkisebb értéket ezek közül!
+
+a = int (input("Kérem az első számot, köszönöm: "))
+b = int (input("Kérem a második számot, köszönöm: "))
+c = int (input("Kérem a harmadik számot, köszönöm: "))
+
+print("¯\_( ͡•‿‿ ͡•)_/¯")
+print("A legkisebb bekért szám a(z) \"" + str(min(a, b, c))+"\"!")
+
