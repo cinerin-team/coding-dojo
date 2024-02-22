60e684bbb551172298676687b5f6704a0a853277
Miklos Parrag <miklos.parrag@ericsson.com>
2024-02-22 14:45:42 +0100

    try expect

--- examples/tryexpect.py
+++ examples/tryexpect.py
@@ -2,5 +2,9 @@ x = input("mondj egy számot\n")
 try:
     a = int(x)
     print("ez egy szam")
-except:
-    print("ez egy string")
+except ValueError:
+    raise Exception("ez egy string")
+else:
+    print("ha nem volt hiba akkor ez is lesz")
+finally:
+    print("ez akkor is kiirodik ha volt hiba és akkor is ha nem")
