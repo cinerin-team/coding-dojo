0160003dd35a5d97240c0cd0ad445aa5da640870
Miklos Parrag <miklos.parrag@ericsson.com>
2024-03-14 11:25:21 +0100

    tdd example

--- test/test_erkmiap_felcsike4_TDD_minta.py
+++ test/test_erkmiap_felcsike4_TDD_minta.py
@@ -11,6 +11,6 @@ class Test(TestCase):
         actual_value_true = oszthato_e_ottel_vagy_harommal(ot)
         actual_value_false = oszthato_e_ottel_vagy_harommal(het)
 
-        self.assertEquals("igen osztható 5-tel vagy 3-mal", actual_value_true)
-        self.assertEquals("nem osztható 5-tel vagy 3-mal", actual_value_false)
+        self.assertEqual("igen osztható 5-tel vagy 3-mal", actual_value_true, "az oszhatóság nincs jól ellenőrizve")
+        self.assertEqual("nem osztható 5-tel vagy 3-mal", actual_value_false, "ha nem oszható akkor nem jó a válasz")
 
